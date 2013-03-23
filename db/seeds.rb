cf44525ba541c10f73842d2cc22b813faae7b3d6
Melissa Weber Mendonca <melissawm@gmail.com>
2013-03-23 12:47:33 -0300

    building seed for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pubs = File.open("#{Rails.root}/db/publicacoes.txt","r").read
+data = pubs.split(/\r\n/)
+
+(0..data.size).step(5).each do |i|
+  professors_name = data[i][0..-2].split(/,/)
+  professors = []
+  professors_name.each do |name|
+    professors << Professor.find_or_create_by_name(name)
+  end
+  p = Publication.new
+  p.professors = professors
+  p.title = data[i+1][0..-2]
+  p.journal = data[i+2][0..-2]
+  p.year = data[i+3][0..-2]
+  p.save
+end
+
